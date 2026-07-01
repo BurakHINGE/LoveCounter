e2c55921cf82613c3d1ebb9f437880cc0ac060ca
BurakHINGE <burakmentese16@gmail.com>
2026-07-01 18:03:22 +0300

    Create Main Class
    Main class has been created.

--- src/loveCounter/mainApp.java
+++ src/loveCounter/mainApp.java
@@ -1,10 +1,21 @@
 package loveCounter;
 
-public class mainApp {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class mainApp extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) {
+		
+		UIManager primaryScene = new UIManager(primaryStage);
+		primaryStage.setScene(primaryScene.mainMenu());
+		primaryStage.setTitle("LOVE APP");
+		primaryStage.show();
+		
 	}
 
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
