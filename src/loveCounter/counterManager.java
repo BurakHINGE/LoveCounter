f54927e2349d36c6b88aa4409a5afcfbc017c33b
BurakHINGE <burakmentese16@gmail.com>
2026-07-22 19:13:56 +0300

    Create CounterManager
    CounterManager class has been created.

--- src/loveCounter/counterManager.java
+++ src/loveCounter/counterManager.java
@@ -1,5 +1,77 @@
 package loveCounter;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class counterManager {
 
+	public static void coffeeCountManager(Button increase, Button decrease, Label count) {
+		
+		increase.setOnMouseClicked(e -> {
+
+            configManager.setCoffeeCount(configManager.getCoffeeCount() + 1);
+            count.setText(String.valueOf(configManager.getCoffeeCount()));
+        });
+		decrease.setOnMouseClicked(e -> {
+
+            configManager.setCoffeeCount(configManager.getCoffeeCount() - 1);
+            count.setText(String.valueOf(configManager.getCoffeeCount()));
+        });
+	}
+	
+	public static void tripCountManager(Button increase, Button decrease, Label count) {
+		
+		increase.setOnMouseClicked(e -> {
+
+            configManager.setTripCount(configManager.getTripCount() + 1);
+            count.setText(String.valueOf(configManager.getTripCount()));
+        });
+		decrease.setOnMouseClicked(e -> {
+
+            configManager.setTripCount(configManager.getTripCount() - 1);
+            count.setText(String.valueOf(configManager.getTripCount()));
+        });
+	}
+
+	public static void strokeCatCountManager(Button increase, Button decrease, Label count) {
+	
+		increase.setOnMouseClicked(e -> {
+
+            configManager.setStrokeCatCount(configManager.getStrokeCatCount() + 1);
+            count.setText(String.valueOf(configManager.getStrokeCatCount()));
+        });
+		decrease.setOnMouseClicked(e -> {
+
+            configManager.setStrokeCatCount(configManager.getStrokeCatCount() - 1);
+            count.setText(String.valueOf(configManager.getStrokeCatCount()));
+        });
+	}
+
+	public static void beatCountManager(Button increase, Button decrease, Label count) {
+	
+		increase.setOnMouseClicked(e -> {
+
+            configManager.setBeatCount(configManager.getBeatCount() + 1);
+            count.setText(String.valueOf(configManager.getBeatCount()));
+        });
+		decrease.setOnMouseClicked(e -> {
+
+            configManager.setCoffeeCount(configManager.getBeatCount() - 1);
+            count.setText(String.valueOf(configManager.getBeatCount()));
+        });
+	}
+	
+	public static void hugCountManager(Button increase, Button decrease, Label count) {
+		
+		increase.setOnMouseClicked(e -> {
+
+            configManager.setHugCount(configManager.getHugCount() + 1);
+            count.setText(String.valueOf(configManager.getHugCount()));
+        });
+		decrease.setOnMouseClicked(e -> {
+
+            configManager.setHugCount(configManager.getHugCount() - 1);
+            count.setText(String.valueOf(configManager.getHugCount()));
+        });
+	}
 }
